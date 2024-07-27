232e51a930f8cbdbbb020843f7ae4662a3668e77
Michael Haberler <mail17@mah.priv.at>
2024-07-27 20:07:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+o import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React + workflows!</p>
+        <p>Hello Vite + React + Demo!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
